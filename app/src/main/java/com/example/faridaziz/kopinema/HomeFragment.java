6c31b3af21f924e4b206f919d5be784520d3361d
unknown <faridazizw@gmail.com>
2019-08-06 05:24:13 +0700

    kopinema

--- app/src/main/java/com/example/faridaziz/kopinema/HomeFragment.java
+++ app/src/main/java/com/example/faridaziz/kopinema/HomeFragment.java
@@ -3,15 +3,19 @@ package com.example.faridaziz.kopinema;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 }
--- app/src/main/java/com/example/faridaziz/kopinema/MainActivity.java
+++ app/src/main/java/com/example/faridaziz/kopinema/MainActivity.java
@@ -1,19 +1,31 @@
 package com.example.faridaziz.kopinema;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView RekomendasiCard, PilihCard, CustomCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RekomendasiCard = findViewById(R.id.menu_rekomendasi);
+        PilihCard = findViewById(R.id.menu_pilih);
+        CustomCard = findViewById(R.id.menu_custom);
+
+        RekomendasiCard.setOnClickListener(this);
+        PilihCard.setOnClickListener(this);
+        CustomCard.setOnClickListener(this);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -41,4 +53,29 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent;
+        switch (v.getId()){
+            case R.id.menu_rekomendasi:{
+                intent = new Intent(this,rekomendasiActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.menu_pilih:{
+                intent = new Intent(this,rekomendasiActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.menu_custom:{
+                intent = new Intent(this,rekomendasiActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
+
+    }
 }
